e7c8f84679553561791ef24d632e0d89958d1b43
sakshizoe <sakshicat12@gmail.com>
2022-05-30 23:02:03 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React  latest verion
         </a>
       </header>
     </div>
